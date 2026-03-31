4a402b8b72f708be17885ac79ea8e74aaaa36ebb
Wolfcrafter321 <Wolfcrafter321@gmail.com>
2026-03-31 18:35:47 +0900

    add mini svelte app

--- _src/App.svelte
+++ _src/App.svelte
@@ -1,24 +1,4 @@
-﻿<!--
-<script>
-    let count = 0
-</script>
-
-<button on:click={() => count++}>
-    {count}
-</button>
-<button class="svelte-button" on:click={() => count++}>
-    {count}
-</button>
-
-<style>
-    button {
-        width: 100px;
-        height: 50px;
-    }
-</style>
--->
-
-<script>
+﻿<script>
 // first load
 import LoadScreen from './screens/FileSelect.svelte'
 import QuestionMode from './screens/QuestionMode.svelte'
@@ -52,17 +32,6 @@ function openEdit() {
     screen = "edit"
 }
 </script>
-
-
-<!-- <button on:click={() => openLoad()}>
-    <p>openLoad</p>
-</button>
-<button on:click={() => openQuestion()}>
-    <p>openQuestion</p>
-</button>
-<button on:click={() => openEdit()}>
-    <p>openEdit</p>
-</button> -->
 
 
 {#if screen === "load"}
---
+++ _src/DemoApp.js
@@ -0,0 +1,4 @@
+﻿import { mount } from 'svelte'
+import App from './DemoApp.svelte'
+
+mount(App, {target: document.getElementById('demo')})
